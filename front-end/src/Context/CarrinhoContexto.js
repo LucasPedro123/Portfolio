97e52add006fdcd3059119ab88eb2e71f6a9b539
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-01-19 08:39:40 -0300

    Deploy 3

--- front-end/src/Context/CarrinhoContexto.js
+++ front-end/src/Context/CarrinhoContexto.js
@@ -1,5 +1,5 @@
 import {createContext, useState, useEffect} from "react";
-
+import data from "./Data/data.json"
 
 const CursosContext = createContext();
 
@@ -8,28 +8,7 @@ function CursosContextProvider( props ){
     const [cursos, setData] = useState([]);
 
     useEffect(() => {
-        const fetchApi = async () => {
-          try {
-            const url = 'http://localhost:8080/';
-            const response = await fetch(url, {
-              method: 'GET',
-            });
-    
-            // Verifica se a resposta foi bem-sucedida
-            if (!response.ok) {
-              throw new Error('Falha na solicitação da API');
-            }
-    
-            // Agora, você pode usar diretamente `response.json()` para obter o JSON
-            const objJson = await response.json();
-            console.log(cursos)
-            setData(objJson);
-          } catch (error) {
-            console.error('Erro na solicitação da API:', error.message);
-          }
-        };
-        
-        fetchApi();
+        setData(data)
       }, []);
     
       console.log(cursos)
