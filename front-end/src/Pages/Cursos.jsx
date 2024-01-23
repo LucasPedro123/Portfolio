c6fab6bd090a40ce2232c5b00c55a20cd4d5a2aa
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-01-22 23:37:10 -0300

    Eliminando imports desnecessários

--- front-end/src/Pages/Cursos.jsx
+++ front-end/src/Pages/Cursos.jsx
@@ -1,13 +1,7 @@
 import React from "react";
 
-import styles from "../Assets/App.module.css"
 
 import NavBar from "../components/NavBar/Navbar";
-import Hero from "../components/Hero/Hero";
-import About from "../components/About/About"
-import Experience from "../components/Experience/Experience";
-import Projects from "../components/Projects/Project"
-import Contact from "../components/Contact/Contact";
 import Cursos from "../components/Cursos/Cursos";
 
 
@@ -15,7 +9,7 @@ import Cursos from "../components/Cursos/Cursos";
 
 function Cursos1() {
   return (
-    <div className={styles.App}>
+    <div >
         <NavBar/>
         <Cursos/>
     </div>
