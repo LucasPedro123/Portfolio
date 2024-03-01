9e5057d3530515f6c824f2c1a25fc93f4e071401
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-03-01 15:49:36 -0300

    perf: changes to routes

--- front-end/src/Routes.jsx
+++ front-end/src/Routes.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import {Suspense, lazy} from "react";
 
+import Home from "./Pages/Home"
+import Cursos1 from "./Pages/Cursos"
+import CursoDetails from "./Pages/CursosDetails"
 
-const Home = lazy(()=> import("./Pages/Home"));
-const Cursos1 = lazy(()=> import("./Pages/Cursos"));
-const CursoDetails = lazy(()=> import("./Pages/CursosDetails"));
+// const Home = lazy(()=> import("./Pages/Home"));
+// const Cursos1 = lazy(()=> import("./Pages/Cursos"));
+// const CursoDetails = lazy(()=> import("./Pages/CursosDetails"));
 
 export const Routes = () => {
     return (
